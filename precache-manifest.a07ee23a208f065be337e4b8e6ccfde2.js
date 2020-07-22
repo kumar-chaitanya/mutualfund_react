4b4800cb808e4c3cdc49efc5db8774e893b3b614
Chaitanya Kumar <16103244chaitanya@gmail.com>
2020-07-23 00:08:28 +0530

    Updates

---
+++ precache-manifest.a07ee23a208f065be337e4b8e6ccfde2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea46257b1cf68f4a743d1bc742d0a75",
+    "url": "/mutualfund_react/index.html"
+  },
+  {
+    "revision": "c8d413611cd930518025",
+    "url": "/mutualfund_react/static/css/main.0ecfd357.chunk.css"
+  },
+  {
+    "revision": "e2a32ef63d7d96aec2cf",
+    "url": "/mutualfund_react/static/js/2.eba72b44.chunk.js"
+  },
+  {
+    "revision": "c8d413611cd930518025",
+    "url": "/mutualfund_react/static/js/main.bc5fdcce.chunk.js"
+  },
+  {
+    "revision": "35780b40f8e5a62b10f5",
+    "url": "/mutualfund_react/static/js/runtime-main.ed6d7300.js"
+  }
+]);
